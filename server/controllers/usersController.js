dcef0709ff846310dd015f1bc4495b8c83d5462b
dekin <dekinlinux@gmail.com>
2023-05-08 04:45:16 +0000

    logout mod

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -130,7 +130,7 @@ const deleteUser = async(req,res)=>{
 
 
 const logOut = (req,res)=>{
-    res.cookie('login','', {maxAge:1})
+    res.cookie('login','', {httpOnly:true,sameSite: 'none',secure: true,maxAge:1})
     res.json({msg:"Logged out successful"})
     res.redirect('/user')
     
