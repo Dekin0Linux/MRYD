557d3ce2dbfbc8d20f61b79aa19d02eb804e5c74
dekin <dekinlinux@gmail.com>
2023-05-24 11:04:27 +0000

    navbar gaps

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -47,8 +47,8 @@ if(getUser){
 
   return (
     <div className='bg-blue-100 w-full sticky top-0 z-[100]'>
-        <div className='container mx-auto text-blue-900 px-10 md:px-0'>
-            <nav className='md:flex justify-between items-center md:py-5 py-2'>
+        <div className='container mx-auto text-blue-900 px-5 md:px-0'>
+            <nav className='md:flex justify-between items-center md:py-5 py-1'>
                 <Link to={'/'}> <img src={logo} alt="logo" className='w-28 animate-bounce'/></Link>
 
               {/* hamburger */}
@@ -62,7 +62,7 @@ if(getUser){
                 {
                   !selected ?
                   (
-                <ul className={'md:flex justify-between md:gap-10 hidden'}>
+                <ul className={'md:flex justify-between md:gap-10 hidden gap-y-10'}>
                     <li className='font-bold '><a href="tel:+233558628473">Toll free</a></li>
                     <Link to={'/'}><li className='font-bold'>Home</li></Link>
                     <Link to={'/mybooking'}><li className='font-bold'>My Booking</li></Link>
@@ -71,12 +71,12 @@ if(getUser){
                     }
                 </ul> )
                 : (
-                <ul className={'md:flex justify-between md:gap-10'}>
+                <ul className={'flex flex-col md:flex-row justify-between md:gap-10 gap-y-4'}>
                   <li className='font-bold '><a href="tel:+233558628473">Toll free</a></li>
-                  <Link to={'/'}><li className='font-bold'>Home</li></Link>
-                  <Link to={'/mybooking'}><li className='font-bold'>My Booking</li></Link>
+                  <Link to={'/'} onClick={()=>setSelected(false)}><li className='font-bold'>Home</li></Link>
+                  <Link to={'/mybooking'} onClick={()=>setSelected(false)} ><li className='font-bold'>My Booking</li></Link>
                   {
-                      loggedIn ? <Link to="/"><li className='font-bold px-2 bg-red-500 py-1 rounded text-white shadow-xl' onClick={logOut}>Logout</li></Link> : ""
+                      loggedIn ? <Link to="/" onClick={()=>setSelected(false)}><li className='font-bold px-2 w-1/2 md:w-auto bg-red-500 py-1 rounded text-white shadow-xl' onClick={logOut}>Logout</li></Link> : ""
                   }
                 </ul>  
                 )
